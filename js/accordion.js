a626d17c6146ad411cc9dee5d58edbc42e0c08de
fffsky <fff.238238238@gmail.com>
2022-12-02 15:04:08 +0900

    閑静

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,5 @@
 const accordions = document.querySelectorAll('.qa-list-item');
+const accordionflow = document.querySelectorAll('.flow-list-item');
 
 accordions.forEach(accordion => {
     // クリックイベントclickされた時
@@ -15,3 +16,19 @@ accordions.forEach(accordion => {
 
     });
 });
+accordionflow.forEach(accordion => {
+    // クリックイベントclickされた時
+    accordion.addEventListener('click', () => {
+        const content = accordion.querySelector('.flow-list-content');
+
+        content.classList.toggle('active');
+
+        if(content.classList.contains('active')) {
+            content.style.height = content.scrollHeight + 'px';
+        } else {
+            content.style.height = '0px';
+        }
+
+    });
+});
+
